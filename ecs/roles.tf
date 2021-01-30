634d5c8f0424d22aed51d7bc8be1f4ff98c34d7f
Siddhu Warrier <siddhu@siddhuw.info>
2021-01-30 22:57:54 +0000

    fix(perms): grant task perms to ECS container to access EC2 (read-only)
    This commit allows the ECS containers running the microservice to make RO calls to EC2, thereby
    making the app functional!

--- ecs/roles.tf
+++ ecs/roles.tf
@@ -19,8 +19,19 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
 }
 
+# ECS task role -- to see the diff between a task role and an execution role, see https://stackoverflow.com/questions/48999472/difference-between-aws-elastic-container-services-ecs-executionrole-and-taskr#:~:text=Referring%20to%20the%20documentation%20you,used%20by%20the%20task%20itself.
+resource "aws_iam_role" "ecs_task_role" {
+  name               = var.ecs_task_role_name
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
+}
+
 # ECS task execution role policy attachment
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_attachment" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_role_ec2_attachment" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
 }
